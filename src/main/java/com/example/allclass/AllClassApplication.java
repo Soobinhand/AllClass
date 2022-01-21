189b76ec45d929cb644aed8d7e044cd055f506c2
Soobinhand <chelsea9528@naver.com>
2022-01-22 01:01:10 +0900

    최초 커밋

---
+++ src/main/java/com/example/allclass/AllClassApplication.java
@@ -0,0 +1,13 @@
+package com.example.allclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllClassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/allclass/AllClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.allclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllClassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
